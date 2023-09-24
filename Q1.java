15caa36d626ca5234c12e40aaa342dd968f2ed54
kpkalyan0717 <145636895+kpkalyan0717@users.noreply.github.com>
2023-09-24 12:27:45 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,10 @@
+// 1.find out the location of a perticular character in the given Sting?
+
+class Q1{
+	public static void main(String[] args){
+		String s1 = "abcdefghij";
+		//           0123456789
+		int i = s1.indexOf('e');
+		System.out.println(i);
+	}
+}
